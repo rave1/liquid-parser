fd5e059d73d86447af07add01f38af8059556922
rave1 <jakubsopel7@gmail.com>
2021-12-07 01:01:15 +0100

    feat: materialui card

--- frontend/src/ItemCard/ItemCard.js
+++ frontend/src/ItemCard/ItemCard.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Container } from './ItemCard.styles';
-import { Button } from '@material-ui/core';
-
+import { Card, CardActions, CardContent, Button, Typography, Avatar, Grid } from '@material-ui/core';
 
 export const ItemCard = (props) => {
     console.log(props.title)
     return (
-        <Container>
-            <img src={props.url}/>
-            <p>{props.title}</p>
-            <select>
-            {props.available.length > 0 && props.available.map((each) => (
-                    <option>{each}</option>
-                ))}
-            </select>
-            <Button variant="contained" color="primary">Submit</Button>
-        </Container>
+        <Card>
+            <CardContent>
+                <Grid container alignItems="center" direction="column">
+
+                <Avatar style={{width: "50px", height: "50px"}} src={props.url}/>
+                <Typography vartian="h5" component="h2">{props.title}</Typography>
+                <select>
+                {props.available.length > 0 && props.available.map((each) => (
+                        <option>{each}</option>
+                    ))}
+                </select>
+                <Button variant="contained" color="primary">Submit</Button>
+                </Grid>
+            </CardContent>
+        </Card>
     )
 }
